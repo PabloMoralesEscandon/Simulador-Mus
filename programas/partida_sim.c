fad856670d14dc676eecd8f24038ada9022e2b58
PabloMoralesEscandon <pablo.morales.escandon@gmail.com>
2026-07-17 11:45:27 +0200

    Semilla más aleatoria para la partida

--- programas/partida_sim.c
+++ programas/partida_sim.c
@@ -1,10 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
+#include <unistd.h>
 
 #include "mus_sim.h"
 
 int main(void) {
-    srand(time(NULL));
+    srand((unsigned)time(NULL) ^ ((unsigned)getpid() << 16));
     fijarVerbosidadSim(1);
     return simularPartidaMus();
 }
